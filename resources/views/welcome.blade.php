174b224af3b8f6b4c7a036accfc458f176cee6ed
Karpinskyi Oleksandr <akarpinskyi@gmail.com>
2020-03-11 21:33:01 +0200

    add modal window

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Deep Dive</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/90ec0072fa.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<header class="Palatino" >
@@ -76,7 +78,56 @@
 
   </div>
 </div>
-<a href="#"><button class="Palatino" id="join"><strong>ПРИЄДНАТИСЬ</strong> </button></a>
+<button class="Palatino" id="join" onclick="show('block')"><strong>ПРИЄДНАТИСЬ</strong> </button>
+
+    <div id="gray" onclick="show('none')"></div>
+    <div id="popup">
+        <div class="container-popup" style="background-image: url('public/img/popup.jpg');">
+            <i class="fas fa-times close" onclick="show('none')"></i>
+            <div class="form">
+                <h2 class="h2 center">Реєстрація</h2>
+                <form action="/" method="POST">
+                    {{ csrf_field() }}
+                    <div class="wrap-input">
+                        <input id="name" class="input" name="name" type="text" placeholder="Ім'я">
+                        <i class="fas fa-user icon"></i>
+                    </div>
+
+                    <div class="wrap-input">
+                        <input id="surname" class="input" name="surname" type="text" placeholder="Прізвище">
+                        <i class="fas fa-user icon"></i>
+                    </div>
+
+                    <div class="wrap-input">
+                        <input id="email" class="input" name="email" type="email" placeholder="E-mail">
+                        <i class="fas fa-envelope icon"></i>
+                    </div>
+
+                    <div class="wrap-input">
+                        <input id="phone_number" class="input" name="phone_number" type="tel" placeholder="Номер телефону">
+                        <i class="fas fa-phone-square-alt icon"></i>
+                    </div>
+
+                    <div class="wrap-input">
+                        <input id="city" class="input" name="city" type="text" placeholder="Місто">
+                        <i class="fas fa-map-marked-alt icon"></i>
+                    </div>
+
+                    {{--                <div class="form-group">--}}
+                    {{--                    <label>--}}
+                    {{--                        {{ Form::checkbox('is_admin', 1, false, ['id' => 'is_admin']) }}--}}
+                    {{--                        Is admin--}}
+                    {{--                    </label>--}}
+                    {{--                </div>--}}
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Приєднатись</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 	</header>
 
 	<div class="Palatino" id="plan">
@@ -294,6 +345,7 @@
   </div>
 </div>
 	</footer>
+    <script src="{{asset('js/app.js')}}"></script>
   <script >
     let slides = document.getElementsByClassName("slide");
 let INDEX = 0;
@@ -353,6 +405,11 @@ document.getElementById("right-button").onclick = function(){
 };
 
 render(INDEX);
+
+    function show(state) {
+        document.querySelector('#popup').style.display = state;
+        document.querySelector('#gray').style.display = state;
+    }
   </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/attach', function () {
+    return view('attach.attach');
+});
+
 
 Auth::routes();
 
