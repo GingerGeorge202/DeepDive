81292f65d47493b916f632853d7cbc9e1dbcc8a3
Karpinskyi Oleksandr <akarpinskyi@gmail.com>
2020-03-28 12:16:37 +0200

    add some style 2

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.admin')
 @section('content')
-    <a href="{{route('feedback.create')}}" class="btn btn-success m-auto add-new">Новий відгук</a>
+    <div class="add-new">
+        <p><b>Додати новий відгук</b></p>
+        <a href="{{route('feedback.create')}}" class="btn btn-success ">Новий відгук</a>
+    </div>
+
     <div class="container-fluid">
         @foreach($feedbacks as $feedback)
 
@@ -8,7 +12,7 @@
 
         <div class="row">
             <div class="col-5 m-auto">
-                <img src="{{$feedback->img ?? 'https://intita.com/images/mainpage/intitaLogo.jpg'}}">
+                <img src="{{$feedback->img ?? 'https://intita.com/images/mainpage/intitaLogo.jpg'}}" width="100%">
             </div>
             <div class="col-1 m-auto">
                 <p>{{$feedback->author}}</p>
@@ -16,8 +20,8 @@
             <div class="col-4 m-auto">
                 <p class="descr" id="p1">{{$feedback->describe}}</p>
             </div>
-            <div class="col-2 m-auto">
-                <a href="{{route('feedback.edit',$feedback->id)}}" class="btn btn-outline-primary m-auto">Редагувати</a>
+            <div class="col-2 m-auto edit-sec">
+                <a href="{{route('feedback.edit',$feedback->id)}}" class="btn btn-outline-primary">Редагувати</a>
                 <form action="{{route('feedback.destroy',$feedback->id)}} " method="post" enctype="multipart/form-data">
                     @csrf
                     @method('DELETE')
@@ -29,12 +33,12 @@
             @else
 
                 <div class="row ">
-                    <div class="col-2 m-auto">
-                        <a href="{{route('feedback.edit',$feedback->id)}}" class="btn btn-outline-primary m-auto">Редагувати</a>
+                    <div class="col-2 m-auto edit-sec">
+                        <a href="{{route('feedback.edit',$feedback->id)}}" class="btn btn-outline-primary">Редагувати</a>
                         <form action="{{route('feedback.destroy',$feedback->id)}} " method="post" enctype="multipart/form-data">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-outline-primary">Видалити</button>
+                            <button type="submit" class="btn btn-outline-danger">Видалити</button>
                         </form>
                     </div>
                     <div class="col-4 m-auto">
@@ -44,7 +48,7 @@
                         <p>{{$feedback->author}}</p>
                     </div>
                     <div class="col-5 m-auto">
-                        <img src="{{$feedback->img ?? 'https://intita.com/images/mainpage/intitaLogo.jpg'}}">
+                        <img src="{{$feedback->img ?? 'https://intita.com/images/mainpage/intitaLogo.jpg'}}" width="100%">
                     </div>
                 </div>
 
